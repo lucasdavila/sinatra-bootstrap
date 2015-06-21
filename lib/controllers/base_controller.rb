215ce717f3e617af12f95dd53d210729370c87d0
Lucas D'Avila <lucas@lucasdavi.la>
2015-06-21 17:06:30 -0300

    Set public_folder

--- lib/controllers/base_controller.rb
+++ lib/controllers/base_controller.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader' if Sinatra::Base.development?
 
 class BaseController < Sinatra::Base
   set :views, 'lib/views'
+  set :public_folder, 'public'
 
   configure :development do
     register Sinatra::Reloader
